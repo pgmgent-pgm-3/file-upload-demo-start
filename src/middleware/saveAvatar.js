0f8d8a09949c70caf24c377767c156d970d6d92d
Frederick Roegiers <frederick@rogerthat.be>
2023-03-21 09:56:13 +0100

    wip

--- src/middleware/saveAvatar.js
+++ src/middleware/saveAvatar.js
@@ -1,3 +1,5 @@
+import sharp from "sharp";
+import { PUBLIC_PATH } from "../consts.js";
 /**
  * The upload middleware
  * a user can upload a file via the browser
@@ -7,8 +9,28 @@
 export const saveAvatar = async (req, res, next) => {
   const file = req.file;
 
+  console.log(file);
+
   // if no file is sent, skip this middleware
   if (!file) next();
+
+  // check if the file is an image
+  if (
+    file.mimetype == "image/png" ||
+    file.mimetype == "image/jpg" ||
+    file.mimetype == "image/jpeg" ||
+    file.mimetype == "image/gif"
+  ) {
+    await sharp(file.buffer)
+      .resize(128, 128, {
+        fit: sharp.fit.cover,
+        withoutEnlargement: true,
+      })
+      .toFile(`${PUBLIC_PATH}/images/avatar/${file.originalname}`);
+  } else {
+    console.log("File type not supported"); // console
+    res.send("File type not supported"); // browser
+  }
 
   next();
 };
